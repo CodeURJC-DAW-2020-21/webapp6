13f5ed40927199721b8e6bb27f75736e5f088583
JSPINDEV <javier.espin@hotmail.es>
2021-03-10 20:30:19 +0100

    6ixShop spring

--- tiendaonline/src/main/java/es/sixshop/model/Order.java
+++
@@ -1,5 +0,0 @@
-package es.sixshop.model;
-
-public class Order {
-
-}
--- tiendaonline/src/main/java/es/sixshop/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package es.sixshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.sixshop.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-}
