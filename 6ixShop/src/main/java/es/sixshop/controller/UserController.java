a8d4f18bbe0da63d0959e8e8a52d7836ae4b0501
JSPINDEV <javier.espin@hotmail.es>
2021-03-11 22:37:59 +0100

    user control and log/sign in

--- 6ixShop/src/main/java/es/sixshop/controller/UserController.java
+++ 6ixShop/src/main/java/es/sixshop/controller/UserController.java
@@ -2,6 +2,7 @@ package es.sixshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import es.sixshop.repository.UserRepository;
 
@@ -10,5 +11,15 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userR;
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/sing_in")
+	public String signin() {
+		return"sign_in";
+	}
 	
 }
