89144f491673a42a84479182d60200452a9dfd88
AlbertoP-2018 <a.pacho.2018@alumnos.urjc.es>
2021-03-28 15:22:29 +0200

    Categoria unificado
    Permite mostrar los productos por categoría con un mismo método

--- 6ixShop/src/main/java/es/sixshop/controller/CategoryController.java
+++ 6ixShop/src/main/java/es/sixshop/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import es.sixshop.model.Product;
 import es.sixshop.model.User;
@@ -42,9 +43,8 @@ public class CategoryController {
 		return "category";
 	}
 	
-	//Estos métodos hay que unificarlos en uno (de momento lo tenemos asi para que )
-	@GetMapping("/category/Movies")
-	public String seeMovies(Model model, HttpSession session, HttpServletRequest request){
+	@GetMapping("/category/{category}")
+	public String seeTVShow(Model model, HttpSession session, HttpServletRequest request, @PathVariable String category){
         //Comprueba si existe una sesión iniciada para cambiar el Header
         if(((Principal)request.getUserPrincipal())!=null) {
             String nickname = request.getUserPrincipal().getName();
@@ -54,95 +54,9 @@ public class CategoryController {
             model.addAttribute("nickname",user.getNickname());
         }
 		Collection<Product> products = productService.findAll();
-		//products = productService.filterByCategory(products, "Movies");
-		model.addAttribute("categoryName","Movies");
+		products = productService.findBycategory(category);
+		model.addAttribute("categoryName",category);
 		model.addAttribute("products",products);
 		return "category";
 	}
-		
-		@GetMapping("/category/TVSeries")
-		public String seeTVShow(Model model, HttpSession session, HttpServletRequest request){
-	        //Comprueba si existe una sesión iniciada para cambiar el Header
-	        if(((Principal)request.getUserPrincipal())!=null) {
-	            String nickname = request.getUserPrincipal().getName();
-	            User user = userR.findByNickname(nickname).orElseThrow();
-
-	            model.addAttribute("user",user);
-	            model.addAttribute("nickname",user.getNickname());
-	        }
-			Collection<Product> products = productService.findAll();
-			//products = productService.filterByCategory(products, "TVSeries");
-			products = productService.findBycategory("TVSeries");
-			model.addAttribute("categoryName","TVSeries");
-			model.addAttribute("products",products);
-			return "category";
-		}
-		
-		@GetMapping("/category/Music")
-		public String seeMusic(Model model, HttpSession session, HttpServletRequest request){
-	        //Comprueba si existe una sesión iniciada para cambiar el Header
-	        if(((Principal)request.getUserPrincipal())!=null) {
-	            String nickname = request.getUserPrincipal().getName();
-	            User user = userR.findByNickname(nickname).orElseThrow();
-
-	            model.addAttribute("user",user);
-	            model.addAttribute("nickname",user.getNickname());
-	        }
-			Collection<Product> products = productService.findAll();
-			//products = productService.filterByCategory(products, "Music");
-			model.addAttribute("categoryName","Music");
-			model.addAttribute("products",products);
-			return "category";
-		}
-		
-		@GetMapping("/category/Photography")
-		public String seePhotography(Model model, HttpSession session, HttpServletRequest request){
-	        //Comprueba si existe una sesión iniciada para cambiar el Header
-	        if(((Principal)request.getUserPrincipal())!=null) {
-	            String nickname = request.getUserPrincipal().getName();
-	            User user = userR.findByNickname(nickname).orElseThrow();
-
-	            model.addAttribute("user",user);
-	            model.addAttribute("nickname",user.getNickname());
-	        }
-			Collection<Product> products = productService.findAll();
-			//products = productService.filterByCategory(products, "Photography");
-			model.addAttribute("categoryName","Photography");
-			model.addAttribute("products",products);
-			return "category";
-		}
-		
-		@GetMapping("/category/Comics")
-		public String seeComics(Model model, HttpSession session, HttpServletRequest request){
-	        //Comprueba si existe una sesión iniciada para cambiar el Header
-	        if(((Principal)request.getUserPrincipal())!=null) {
-	            String nickname = request.getUserPrincipal().getName();
-	            User user = userR.findByNickname(nickname).orElseThrow();
-
-	            model.addAttribute("user",user);
-	            model.addAttribute("nickname",user.getNickname());
-	        }
-			Collection<Product> products = productService.findAll();
-			//products = productService.filterByCategory(products, "Comics");
-			model.addAttribute("categoryName","Comics");
-			model.addAttribute("products",products);
-			return "category";
-		}
-		
-		@GetMapping("/category/Custom")
-		public String seeCustom(Model model, HttpSession session, HttpServletRequest request){
-	        //Comprueba si existe una sesión iniciada para cambiar el Header
-	        if(((Principal)request.getUserPrincipal())!=null) {
-	            String nickname = request.getUserPrincipal().getName();
-	            User user = userR.findByNickname(nickname).orElseThrow();
-
-	            model.addAttribute("user",user);
-	            model.addAttribute("nickname",user.getNickname());
-	        }
-			Collection<Product> products = productService.findAll();
-			//products = productService.filterByCategory(products, "Custom");
-			model.addAttribute("categoryName","Custom");
-			model.addAttribute("products",products);
-			return "category";
-		}
 }
