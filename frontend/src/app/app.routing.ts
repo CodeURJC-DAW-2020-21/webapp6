2064324c8a3c0815a96800bea1c704e973ecf6c8
AlbertoP-2018 <a.pacho.2018@alumnos.urjc.es>
2021-04-27 10:55:00 +0200

    Componentes y routing
    Componentes y routing

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,20 +1,38 @@
 import { RouterModule } from '@angular/router';
 
+// **** PRODUCTS ****
 import { PrincipalComponent } from './components/products/principal.component';
+import { ErrorComponent } from './components/products/error.component';
+import { SingleProductComponent } from './components/products/single-product/single-product.component';
+import { EditProductComponent } from './components/products/edit-product/edit-product.component';
+// **** CATEGORY ****
 import { CategoryComponent } from './components/category/category.component';
-//import { BookListComponent } from './book-list.component';
-//import { BookDetailComponent } from './book-detail.component';
+// **** USER ****
+import { LoginComponent } from './components/user/login/login.component';
+import { SignInComponent } from './components/user/sign-in/sign-in.component';
+// **** PROFILE ****
+import { ProfileComponent } from './components/profile/profile.component';
+// **** CART ****
+import { CartComponent } from './components/cart/cart.component';
+import { CardPaymentComponent } from './components/cart/cardPayment/cardPayment.component';
 
 const appRoutes = [
-    { path: 'new', component: PrincipalComponent,  },
+    // **** PRODUCTS ****
+    { path: 'new', component: PrincipalComponent },
+    { path: 'new/single-product', component: SingleProductComponent },
+    { path: 'new/edit-product', component: EditProductComponent },
+    { path: 'new/error', component: ErrorComponent },
+    // **** CATEGORY ****
     { path: 'new/category', component: CategoryComponent},
+    // **** USER ****
+    { path: 'new/login', component: LoginComponent},
+    { path: 'new/signIn', component: SignInComponent},
+    // **** PROFILE ****
+    { path: 'new/profile', component: ProfileComponent},
+    // **** CART ****
+    { path: 'new/cart', component: CartComponent},
+    { path: 'new/cart/cardPayment', component: CardPaymentComponent},
     { path: '', redirectTo: 'new', pathMatch: 'full' }
   ]
-/*
-const appRoutes = [
-  { path: 'book/:id', component: BookDetailComponent,  },
-  { path: 'books', component: BookListComponent },
-  { path: '', redirectTo: 'books', pathMatch: 'full' }^
-]*/
 
 export const routing = RouterModule.forRoot(appRoutes);
