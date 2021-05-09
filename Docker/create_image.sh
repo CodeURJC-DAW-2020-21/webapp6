4cd35b0c2167418afbf6e44522a8317d05c65ec8
Fezzik23 <58087844+Fezzik23@users.noreply.github.com>
2021-05-09 22:13:05 +0200

    cambios docker

--- Docker/create_image.sh
+++ Docker/create_image.sh
@@ -5,8 +5,9 @@ mkdir new
 cd ../../../../../frontend/
 npm install
 ng build --prod --base-href="/new/"
-cp -r dist/data ../backend/src/main/resources/static/new/
+cp -r dist/frontend ../backend/src/main/resources/static/new/
 cd ../backend
+sudo docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app node:latest /bin/bash -c "npm install && npm run build"
 sudo docker run --rm -v "$PWD":/data -w /data maven mvn package
 cp ./target/6ixShop-0.1.0-SNAPSHOT.jar ../Docker
 cd ../Docker
